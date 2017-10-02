a38a05d96a61026d0d6eca4f72c9e754b8a853b9
Jacob Bailey <jakebailey.jb96@gmail.com>
2017-10-02 17:42:39 -0400

    Added Facebook Authentication
    You can click the facebook button and login to facebook and firebase

--- Social-Network/View/SignInVC.swift
+++ Social-Network/View/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,34 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtn(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Jake: Unable to authenticate with facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("Jake: User cancelled Facebook Authentication")
+            } else {
+                print("Jake: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("Jake: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("Jake: Successfully authenticated with Firebase")
+            }
+        }
+    }
+    
+    
+    
 }
 
