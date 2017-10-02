1ad01abb055499dead057cf47d81dec10bc38576
Jacob Bailey <jakebailey.jb96@gmail.com>
2017-10-02 16:11:00 -0400

    Added some minor tweaks to UI
    The UI has some more shadows and rounded corners.

---
+++ Social-Network/Util/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Social-Network
+//
+//  Created by Jacob Bailey on 10/2/17.
+//  Copyright © 2017 Jacob Bailey. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
---
+++ Social-Network/View/FancyView.swift
@@ -0,0 +1,22 @@
+//
+//  FancyView.swift
+//  Social-Network
+//
+//  Created by Jacob Bailey on 10/2/17.
+//  Copyright © 2017 Jacob Bailey. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+}
